4dad44fd7e202825a1dc857649e8ebc522ac1f83
Siddalinga <siddaling.gkoudi@gmail.com>
2022-03-29 18:39:10 +0530

    Added google authentication

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const cors = require('cors')
 const body_parser = require("body-parser")
 const connect = require("./src/configs/db")
+const gameSchema= require("./src/controllers/game.controller")
+const cartSchema= require("./src/controllers/cart.controller")
+const userSchema= require("./src/controllers/user.controller")
+const {register, login,newToken} = require("./src/controllers/auth_controller")
+const passport = require("./src/configs/google-oauth");
+
 const app = express()
 
 require('dotenv').config();
@@ -10,10 +16,7 @@ app.use(body_parser.json())
 
 app.use(cors())
 
-const gameSchema= require("./src/controllers/game.controller")
-const cartSchema= require("./src/controllers/cart.controller")
-const userSchema= require("./src/controllers/user.controller")
-const {register, login} = require("./src/controllers/auth_controller")
+
 
 app.use("/games",gameSchema)
 app.use("/cart",cartSchema)
@@ -21,6 +24,32 @@ app.use("/user",userSchema)
 app.post("/register",register);
 app.post("/login",login);
 
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
+  
+  app.get(
+    "/auth/google",
+    passport.authenticate("google", { scope: ["email", "profile"] })
+  );
+  
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google", {
+      failureRedirect: "/auth/google/failure",
+    }),
+    (req, res) => {
+      const { user } = req;
+      const token = newToken(user);
+  
+      return res.send({ user, token });
+    }
+  );
+  
 app.listen(PORT, async () =>{
 
     try {
@@ -30,5 +59,4 @@ app.listen(PORT, async () =>{
     } catch (error) {
         console.log('error:', error.message)
     }
-
 })
